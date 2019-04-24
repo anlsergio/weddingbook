124f66ec4909eba04743b1b8675fb155c8607e4a
Anderson Luis Sergio <anderson.als04@gmail.com>
2019-04-24 18:25:39 -0300

    Added S3 Support

--- weddingbook/urls.py
+++ weddingbook/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('', include('gallery.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
