14304938ded6cc7633512ff39e5cc140b7da7c0d
Anderson Luis Sergio <anderson.als04@gmail.com>
2019-04-26 22:44:23 -0300

    Feat: Fix number of elements on pagination

--- gallery/views.py
+++ gallery/views.py
@@ -24,7 +24,7 @@ class ImageListView(ListView):
     model = Image
     template_name = 'gallery/home.html'
     context_object_name = 'images'
-    paginate_by = '1'
+    paginate_by = '6'
 
     def get_queryset(self):
         return Image.objects.filter(approved=True).order_by('-date_posted')
